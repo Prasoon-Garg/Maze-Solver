a28190f9a8c5cb3d0ecbdf88bde770b91368d05d
Snehit Gupta <snehit.g@research.iiit.ac.in>
2022-11-07 00:07:39 +0530

    gate find + basic extraction

---
+++ src/path_find.py
@@ -0,0 +1,30 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.9.12 ('sk')",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.9.12"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "88a753c43b17087171adffffa8df0f44dacb588d76583af103687dee8b64c069"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
