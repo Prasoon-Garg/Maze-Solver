176a4b0f85168825482daf510bef48346e8fa05b
Prashant Gupta <82945846+PrashantGupta21@users.noreply.github.com>
2022-11-23 20:39:15 +0530

    Add files via upload

--- src1/test.py
+++ src1/test.py
@@ -7,17 +7,20 @@ from gate_find import *
 from path_find import *
 from solution_overlay import *
 
-org_img = cv2.imread('easy.jpg')
+org_img = cv2.imread('test1.png')
+org_img = cv2.copyMakeBorder(org_img,3,3,3,3,cv2.BORDER_CONSTANT, None, value = (255,255,255))
 
 preprocess_img = preprocess(org_img)
 extracted_maze = morph(preprocess_img)
 gates = gate_find(preprocess_img)
 
-gates[0][1] = 110
-gates[1][1] = 490
+# gates[0][1] = 110
+# gates[1][1] = 490
 
-gates[0][0] = 490
-gates[1][0] = 110
+# gates[0][0] = 490
+# gates[1][0] = 110
+
+print(gates)
 
 path_res = final_path(extracted_maze,gates[0],gates[1])
 
