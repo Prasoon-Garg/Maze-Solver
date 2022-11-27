4d8a7a2316ddef18d6987608c03211e88075922e
Prasoon-Garg <73867666+Prasoon-Garg@users.noreply.github.com>
2022-11-28 00:06:48 +0530

    Add files via upload

--- src/solution_overlay.py
+++ src/solution_overlay.py
@@ -11,23 +11,23 @@ def sol_overlay(org_image,path,preprocess_image):
         
         if(abs(prev_x-curr_x)==1):
             for y in range(curr_y, curr_y-5,-1):
-                if(y<0 or image[curr_x][y][0]==0):
+                if(y<0 or preprocess_image[curr_x][y]==255):
                     break
                 image[curr_x][y] = (255,0,0)
                 
             for y in range(curr_y, curr_y+5):
-                if(y>=w or image[curr_x][y][0]==0):
+                if(y>=w or preprocess_image[curr_x][y]==255):
                     break
                 image[curr_x][y] = (255,0,0)
         
         else:
             for x in range(curr_x, curr_x-5,-1):
-                if(x<0 or image[x][curr_y][0]==0):
+                if(x<0 or preprocess_image[x][curr_y]==255):
                     break
                 image[x][curr_y] = (255,0,0)
                 
             for x in range(curr_x, curr_x+5):
-                if(x>=h or image[x][curr_y][0]==0):
+                if(x>=h or preprocess_image[x][curr_y]==255):
                     break
                 image[x][curr_y] = (255,0,0)
 
