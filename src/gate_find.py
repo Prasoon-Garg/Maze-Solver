0042296061ebb41cbf6f13144a072f7eea544175
Snehit Gupta <snehit.g@research.iiit.ac.in>
2022-11-28 20:36:00 +0530

    final

--- src/gate_find.py
+++ src/gate_find.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as pcnt
+import matplotlib.pyplot as plt
 
 from prep_maze import *
 from extract_maze import *
--- src/maze_solve.py
+++ src/maze_solve.py
@@ -9,7 +9,11 @@ from gate_find import *
 from path_find import *
 from solution_overlay import *
 
-org_img = cv2.imread('../data/inputs/test12.jpg')
+ipath = './data/inputs/'
+opath = './data/outputs/'
+file = 'rotated.jpg'
+
+org_img = cv2.imread(''.join([ipath, file]))
 org_img = cv2.cvtColor(org_img,cv2.COLOR_BGR2RGB)
 
 preprocess_img = preprocess(org_img)
@@ -21,6 +25,6 @@ gates = gate_find(preprocess_img,corners)
 path_res = final_path(extracted_maze,gates[0],gates[1])
 final_res = sol_overlay(org_img,path_res,preprocess_img)
 
-
 plt.imshow(final_res)
+plt.imsave(''.join([opath, file]), final_res)
 plt.show()
